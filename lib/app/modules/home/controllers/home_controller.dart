2522fbf4f56af6159f3844324cdc744836234f1d
Serkhani <dak.boniface@gmail.com>
2022-12-22 06:03:37 +0000

    added circle, line and animCon

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,11 +1,18 @@
+import 'package:flutter/animation.dart';
 import 'package:get/get.dart';
+import 'dart:math' as math;
 
-class HomeController extends GetxController {
-  //TODO: Implement HomeController
-
-  final count = 0.obs;
+class HomeController extends GetxController with GetTickerProviderStateMixin {
+  late AnimationController lineAnimcon;
+  late Animation lineAnim;
   @override
   void onInit() {
+    lineAnimcon = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 2),
+        lowerBound: 0.0,
+        upperBound: 100.0);
+    lineAnim = Tween(begin: 0.0, end: 100.0).animate(lineAnimcon);
     super.onInit();
   }
 
@@ -18,6 +25,4 @@ class HomeController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+import 'dart:math' as math;
+
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -14,11 +17,57 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        child: Stack(
+          children: [
+            CustomPaint(
+              painter: CirclePainter(),
+            ),
+            GetBuilder<HomeController>(
+              builder: (controller) {
+                return CustomPaint(
+                  painter: LinePainter(),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
   }
 }
+
+class LinePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint painter = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0;
+
+    canvas.drawLine(Offset.zero, const Offset(0.0, 100.0), painter);
+  }
+
+  @override
+  bool shouldRepaint(LinePainter oldDelegate) => false;
+}
+
+class CirclePainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint painter = Paint()
+      ..color = Colors.red
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 3.0;
+    canvas.drawCircle(Offset.zero, 100.0, painter);
+  }
+
+  @override
+  bool shouldRepaint(CirclePainter oldDelegate) => false;
+}
+//  animatePath(Rect oval, Paint paint, Canvas canvas, double progress) {
+//     canvas.drawArc(oval, 0.0, progress, true, paint);
+    // PathMetrics pathMetrics = path.computeMetrics();
+    // for (PathMetric pathMetric in pathMetrics) {
+    //   Path path_ = pathMetric.extractPath(0.0, pathMetric.length * progress);
+    //   canvas.drawPath(path_, paint);
+    // }
